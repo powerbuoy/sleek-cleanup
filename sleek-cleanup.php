36f4e84f15124d78e5edf7282a9ac762ec28503d
AL <powerbuoy@gmail.com>
2019-10-02 14:10:08 +0200

    Move comment-form stuff here

--- sleek-cleanup.php
+++ sleek-cleanup.php
@@ -76,6 +76,48 @@ if (!is_admin()) {
 		return str_replace("'", '"', str_replace("type='text/javascript' ", '', $html));
 	});
 
+	#######################
+	# Cleanup comment form
+	add_filter('comment_form_defaults', function ($args) {
+		# A little nicer output
+		$args['id_form'] = null;
+		$args['class_form'] = null;
+		$args['title_reply_before'] = '<h2>';
+		$args['title_reply_after'] = '</h2>';
+		$args['format'] = 'html5';
+
+		# Add placeholders to fields
+		if (get_theme_support('sleek-comment-form-placeholders')) {
+			# All fields we want to add placeholders to
+			$fieldsToReplace = [
+				'author' => __('Name'),
+				'email' => __('Email'),
+				'url' => __('Website')
+			];
+
+			foreach ($fieldsToReplace as $field => $value) {
+				# Add asterisk if required
+				$required = strstr($args['fields'][$field], 'required') ? ' *' : '';
+
+				# Insert placeholder
+				$args['fields'][$field] = str_replace(
+					'name="' . $field . '"',
+					'name="' . $field . '" placeholder="' . $value . $required . '"',
+					$args['fields'][$field]
+				);
+			}
+
+			# Comment field is special and always required
+			$args['comment_field'] = str_replace(
+				'name="comment"',
+				'name="comment" placeholder="' . _x('Comment', 'noun') . ' *"',
+				$args['comment_field']
+			);
+		}
+
+		return $args;
+	});
+
 	##############
 	# Cleanup head
 	# TODO: Investigate and document all of these
